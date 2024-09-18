6755ec106b7b6df2f1437beadb044983fa3aa958
vavarvariuk <vasya.varvariuk.27@gmail.com>
2024-09-18 17:10:50 +0300

    solution py-car-wash-station

--- app/main.py
+++ app/main.py
@@ -1,8 +1,40 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self, comfort_class, clean_mark, brand):
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self, distance_from_city_center, clean_power,
+                 average_rating, count_of_ratings):
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def calculate_washing_price(self, car: Car):
+        price_wash = (car.comfort_class
+                      * (self.clean_power - car.clean_mark)
+                      * (self.average_rating / self.distance_from_city_center))
+
+        return round(price_wash, 1)
+
+    def wash_single_car(self, car: Car):
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
+
+    def serve_cars(self, list_car: list):
+        list_price = []
+        for car in list_car:
+            if self.clean_power > car.clean_mark:
+                list_price.append(CarWashStation.calculate_washing_price(self,
+                                                                         car))
+                CarWashStation.wash_single_car(self, car)
+        return round(sum(list_price), 1)
+
+    def rate_service(self, number: int):
+        self.average_rating = round((self.count_of_ratings
+                                     * self.average_rating + number)
+                                    / (self.count_of_ratings + 1), 1)
+        self.count_of_ratings += 1
